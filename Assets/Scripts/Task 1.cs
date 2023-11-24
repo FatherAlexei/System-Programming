447d6db1e6ab5d415135826a4a53b5bddcb97260
FatherAlexei <68125662+FatherAlexei@users.noreply.github.com>
2023-11-23 19:13:29 -0800

    Did homework

--- Assets/Scripts/Task 1.cs
+++ Assets/Scripts/Task 1.cs
@@ -1,29 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
 
 public class Task1 : MonoBehaviour
 {
-    Unit unit;
     void Start()
     {
-        Unit unit = new Unit();
-    }
-
-    public void ReceiveHealing()
-    {
-        StopCoroutine(MyCoroutine());
-        StartCoroutine(MyCoroutine());
-    }
-
-    IEnumerator MyCoroutine()
-    {
-        for (int i = 0; i < 6; i++)
+        NativeArray<int> num = new NativeArray<int>(new int[] { 1,2,3,20,30,40 }, Allocator.Persistent);
+        MyJob myJob = new MyJob()
         {
-            if (unit.hp >= unit.maxHp)
-               yield break;
-            unit.hp += 5;
-            yield return new WaitForSeconds(0.5f);
+            result = num
+        };
+        JobHandle jobHandle = myJob.Schedule();
+        jobHandle.Complete();
+        for (int i = 0; i < num.Length; i++)
+            Debug.Log(num[i]);
+    }
+
+    public struct MyJob : IJob
+    {
+        public NativeArray<int> result;
+        public void Execute()
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] >= 10)
+                    result[i] = 0;
+            }
         }
     }
 }
--- Assets/Scripts/Task2.cs
+++ Assets/Scripts/Task2.cs
@@ -2,33 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
 
 public class Task2 : MonoBehaviour
 {
-    public CancellationTokenSource cancellationTokenSource;
-    // Start is called before the first frame update
     void Start()
     {
-        cancellationTokenSource = new CancellationTokenSource();
-        CancellationToken cancelToken = cancellationTokenSource.Token;
-        UnitTasksAsync(cancelToken);
+        NativeArray<Vector3> arr1 = new NativeArray<Vector3>(new Vector3[] { Vector3.one,Vector3.one,Vector3.up }, Allocator.Persistent);
+        NativeArray<Vector3> arr2 = new NativeArray<Vector3>(new Vector3[] { Vector3.down, Vector3.left, Vector3.right }, Allocator.Persistent);
+        NativeArray<Vector3> result = new NativeArray<Vector3>(new Vector3[] { Vector3.zero, Vector3.zero, Vector3.zero }, Allocator.Persistent);
+        MyJob myJob = new MyJob()
+        {
+            arr1 = arr1,
+            arr2 = arr2,
+            result = result
+        };
+        JobHandle jobHandle = myJob.Schedule(arr1.Length,0);
+        jobHandle.Complete();
+        for (int i = 0; i < result.Length; i++)
+            Debug.Log(result[i]);
     }
 
-    void UnitTasksAsync(CancellationToken cancelToken)
+    public struct MyJob : IJobParallelFor
     {
-        Task task1 = Task.Run(() => Unit1Async(cancelToken));
-        Task task2 = Task.Run(() => Unit2Async(cancelToken));
-    }
-    async Task Unit1Async(CancellationToken cancelToken)
-    {
-        await Task.Delay(1000);
-        Debug.Log("Task 1 ended");
-    }
-    async Task Unit2Async(CancellationToken cancelToken)
-    {
-        for (int i = 0; i < 60; i++)
-            await Task.Yield();
-        Debug.Log("Task 2 ended");
+        public NativeArray<Vector3> arr1;
+        public NativeArray<Vector3> arr2;
+        public NativeArray<Vector3> result;
+        public void Execute(int index)
+        {
+            result[index] = arr1[index] + arr2[index];
+        }
     }
 }
